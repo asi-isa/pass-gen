f9a2cb5deacf29c50457d2a219d434db33e16cc8
asi-isa <ali.inceoglu@protonmail.com>
2022-12-19 08:25:37 +0100

    style checkbox

--- src/components/Option.vue
+++ src/components/Option.vue
@@ -1,21 +1,32 @@
 <script setup>
-const { name, value } = defineProps({
+import { computed } from "vue";
+import { IconCheck } from "@iconify-prerendered/vue-mdi";
+
+const { name, value, modelValue } = defineProps({
   name: String,
   value: String,
   modelValue: Array,
 });
 
 defineEmits(["update:modelValue"]);
+
+const checked = computed(() => {
+  return modelValue.includes(value);
+});
 </script>
 
 <template>
-  <div class="flex gap-2">
-    <input
-      type="checkbox"
-      :value="value"
-      :checked="modelValue.includes(value)"
-      @change="$emit('update:modelValue', $event.target.value)"
-    />
+  <div
+    class="flex items-center gap-2 cursor-pointer"
+    @click="$emit('update:modelValue', value)"
+  >
+    <div
+      class="w-4 h-4 border border-[var(--accent)] rounded-sm flex items-center content-center transition-colors duration-500"
+      :class="{ 'bg-[var(--accent)]': checked }"
+    >
+      <IconCheck v-if="checked" class="text-[var(--color-background)]" />
+    </div>
+
     <p class="name">{{ name }}</p>
   </div>
 </template>
